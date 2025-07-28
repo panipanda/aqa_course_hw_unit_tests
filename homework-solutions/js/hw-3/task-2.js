72d73baa1d103c617ae7b6a5983401ac8fea325f
Tatiana Kashitsyna <tkashitsyna@gmail.com>
2025-07-28 21:26:22 +0200

    homework 3 completed

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,8 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+let firstInput = 7; // Любое целое положительное число от 1 до 9
+firstInput = String(firstInput); // Преобразуем в строку для конкатенации
+let secondInput = firstInput + firstInput;
+let thirdInput = firstInput + firstInput + firstInput;
+console.log(Number(firstInput) + Number(secondInput) + Number(thirdInput));
