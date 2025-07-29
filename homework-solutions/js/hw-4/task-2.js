1c012d766f9a79ad50618c90143ba172bf9aac0b
Tatiana Kashitsyna <tkashitsyna@gmail.com>
2025-07-29 23:05:00 +0200

    hw-4 completed

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,20 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+let minAge = 18;
+let maxAge = 60;
+let age = 25; // To be changed for testing
+if (typeof age !== "number") {
+    console.log("Incorrect data type");
+} // Check if age is not a number
+else {
+    if (age < minAge) {
+        console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+    } else if (age >= minAge && age < maxAge) {
+        console.log("Welcome!");
+    } else if (age > maxAge) {
+        console.log("Keep calm and look Culture channel.");
+    } else {
+        console.log("Technical work.");
+    }
+}  
